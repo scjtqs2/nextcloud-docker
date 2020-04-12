3d5432f6cca87be2cc485a56283bd7fc8bb9bfbd
scjtqs <scjtqs@qq.com>
2020-04-13 02:25:03 +0800

    add for buildx

---
+++ build_multi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+## docker buildx create --use --name mybuilder
+docker buildx build --tag scjtqs/nextcloud:latest --platform linux/amd64,linux/arm64,linux/arm/v7,linux/386,linux/ppc64le --push .
