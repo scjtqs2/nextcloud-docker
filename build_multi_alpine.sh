e2c45e150f5e1c66b371e4ff57f6345b5d897c63
scjtqs <scjtqs@qq.com>
2020-10-07 20:06:31 +0800

    add alpine-fpm

---
+++ build_multi_alpine.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx create --use --name mybuilder2
+docker buildx build --tag scjtqs/nextcloud:alpine --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le --push -f Dockerfile.fpm.alpine .
+docker buildx rm mybuilder2
