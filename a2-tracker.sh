627164cc7d38b279d31f86f0d5c354262495729e
scjtqs <scjtqs@qq.com>
2020-10-21 22:03:50 +0800

    aria2 bt-tracker

--- a2-tracker.sh
+++ a2-tracker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #添加以下代码
 path=`pwd`/aria2.conf
-list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+list=`wget -qO- https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_all_ip.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" $path`" ]; then
     sed -i '$a bt-tracker='${list}  $path
         echo add......
