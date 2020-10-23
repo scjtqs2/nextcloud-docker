7ee4858afa4d970d7db95d7f773effae68a43859
scjtqs <scjtqs@qq.com>
2020-10-24 01:12:33 +0800

    fix

--- kintohub_web/init.sh
+++ kintohub_web/init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST http://127.0.0.1/index.php
+curl -X POST http://127.0.0.1/index.php?INSTALL=2 -F "install=true;adminlogin=$NEXTCLOUD_ADMIN_USER;adminpass=$NEXTCLOUD_ADMIN_PASSWORD;adminpass-clone=$NEXTCLOUD_ADMIN_PASSWORD"
