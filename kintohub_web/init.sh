072619428e99f03fda64f4fe9b28a2b39a1bb270
scjtqs <scjtqs@qq.com>
2020-10-24 01:25:04 +0800

    fix

--- kintohub_web/init.sh
+++ kintohub_web/init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST 'http://127.0.0.1/index.php?INSTALL=2' -H 'Content-Type: application/x-www-form-urlencoded'  --data-urlencode 'install=on' --data-urlencode "adminlogin=$NEXTCLOUD_ADMIN_USER" --data-urlencode "adminpass=$NEXTCLOUD_ADMIN_PASSWORD" --data-urlencode "adminpass-clone=$NEXTCLOUD_ADMIN_PASSWORD"
+curl -X POST 'http://127.0.0.1/index.php?INSTALL=2' -H 'Content-Type: application/x-www-form-urlencoded'  --data-urlencode 'install=true' --data-urlencode "adminlogin=$NEXTCLOUD_ADMIN_USER" --data-urlencode "adminpass=$NEXTCLOUD_ADMIN_PASSWORD" --data-urlencode "adminpass-clone=$NEXTCLOUD_ADMIN_PASSWORD"
