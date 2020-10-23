b755192cda9a0f7d1f4b774789409a8185f766f5
scjtqs <scjtqs@qq.com>
2020-10-24 01:15:49 +0800

    fix

--- config.php
+++ config.php
@@ -31,7 +31,7 @@ $CONFIG = array (
   'dbtableprefix' => 'oc_',
   'dbuser' => getenv('MYSQL_USER')?:(getenv('POSTGRES_USER')?:'') ,
   'dbpassword' => getenv('MYSQL_PASSWORD')?:(getenv('POSTGRES_PASSWORD')?:''),
-  'installed' => ($_GET['INSTALL']==2)?false:(getenv('INSTALLED')?true:false),
+  'installed' => ($_GET['INSTALL']==2)?false:(($_POST['install']=='on')?false:(getenv('INSTALLED')?true:false)),
   'overwriteprotocol' => 'https',
 //  'overwrite.cli.url'=>'https://'.getenv('NEXTCLOUD_TRUSTED_DOMAINS')?:getenv('DOMAIN'),
   'loglevel' => 3,
