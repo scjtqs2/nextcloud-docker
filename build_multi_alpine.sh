2da3cc0da4e635c18fefdc380a7216893d095876
scjtqs <scjtqs@qq.com>
2021-02-08 11:57:45 +0800

    阿里云镜像

--- build_multi_alpine.sh
+++ build_multi_alpine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --use --name mybuilder2
+docker buildx create --use --name mybuilder182
 docker buildx build --tag scjtqs/nextcloud:v18-alpine --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le --push -f Dockerfile.fpm.alpine .
-docker buildx rm mybuilder2
+docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/nextcloud:v18-alpine --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le --push -f Dockerfile.fpm.alpine .
+docker buildx rm mybuilder182
