fa625a96721625d52e187579b088efca42ccf24c
scjtqs <scjtqs@qq.com>
2021-02-08 11:51:37 +0800

    添加 阿里云镜像地址

--- build_multi.sh
+++ build_multi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuilder
-docker buildx build --tag scjtqs/nextcloud:v20-latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le --pull --push .
+docker buildx build --tag scjtqs/nextcloud:v20-latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le  --push .
+docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/nextcloud:v20-latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le  --push .
 docker buildx rm mybuilder
