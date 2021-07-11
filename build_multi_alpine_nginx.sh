ec3a7e8248f37e3ce491e98228048856f650e50e
scjtqs <scjtqs@qq.com>
2021-07-11 14:57:05 +0800

    qemu

--- build_multi_alpine_nginx.sh
+++ build_multi_alpine_nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuilder3
 docker buildx build --tag scjtqs/nextcloud:alpine-nginx --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le --push -f Dockerfile.fpm.alpine.nginx .
 docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/nextcloud:alpine-nginx --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le --push -f Dockerfile.fpm.alpine.nginx .
