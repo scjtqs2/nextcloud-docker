70b79b2568083226acf7287bccc8ad445b0008f5
scjtqs <scjtqs@qq.com>
2021-12-23 13:54:09 +0800

    fix v22

--- build_multi.sh
+++ build_multi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuilder
-docker buildx build --tag scjtqs/nextcloud:latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le  --push  .
-docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/nextcloud:latest --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le  --push .
+docker buildx build --tag scjtqs/nextcloud:v22 --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le  --push  .
+docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/nextcloud:v22 --platform linux/amd64,linux/arm64,linux/arm/v6,linux/386,linux/ppc64le  --push .
 docker buildx rm mybuilder
