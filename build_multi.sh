dd081b6359ee5ce86c3e5fc0d9c39147665058bc
scjtqs <scjtqs@qq.com>
2024-05-07 17:41:27 +0800

    fix: sh

--- build_multi.sh
+++ build_multi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-TAG=s1
 #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuilder
-docker buildx build --tag scjtqs/nextcloud:v${TAG} --platform linux/amd64,linux/arm64,linux/arm/v7  --push  .
+docker buildx build --tag scjtqs/nextcloud:latest --platform linux/amd64,linux/arm64,linux/arm/v7  --push  .
 # docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/nextcloud:latest --platform linux/amd64,linux/arm64,linux/arm/v7  --push .
 docker buildx rm mybuilder
--- build_multi_alpine.sh
+++ build_multi_alpine.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-TAG=s1
 #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name mybuilder2
-docker buildx build --tag scjtqs/nextcloud:v${TAG}alpine --platform linux/amd64,linux/arm64,linux/arm/v7  --push -f Dockerfile.fpm.alpine .
+docker buildx build --tag scjtqs/nextcloud:alpine --platform linux/amd64,linux/arm64,linux/arm/v7  --push -f Dockerfile.fpm.alpine .
 #docker buildx build --tag registry.cn-hangzhou.aliyuncs.com/scjtqs/nextcloud:alpine --platform linux/amd64,linux/arm64,linux/arm/v7   --push -f Dockerfile.fpm.alpine .
 docker buildx rm mybuilder2
